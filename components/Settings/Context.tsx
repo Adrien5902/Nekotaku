378422161982596aecd73d78b48b5d3328d39f0f
Adrien5902 <adrien.m5902@gmail.com>
2024-12-01 20:05:23 +0100

    Save settings to disk #46

--- components/Settings/Context.tsx
+++ components/Settings/Context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { DefaultSettings, type Settings } from "./types";
 import * as FileSystem from "expo-file-system";
 import { usePromise } from "@/hooks/usePromise";
@@ -14,11 +14,28 @@ export const ChangeSettingsContext = createContext<
 export default function SettingsProvider({
 	children,
 }: { children: React.ReactNode }) {
-	const { data } = usePromise(async () => {
+	const { data, loading } = usePromise(async () => {
 		return JSON.parse(await FileSystem.readAsStringAsync(SETTINGS_LOCATION));
 	});
 
-	const [settings, setSettings] = useState(data ?? DefaultSettings);
+	const [settings, setSettings] = useState(DefaultSettings);
+
+	useEffect(() => {
+		if (data) setSettings(data);
+	}, [data]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const json = JSON.stringify(settings);
+				await FileSystem.writeAsStringAsync(SETTINGS_LOCATION, json);
+			} catch (error) {
+				// TODO: handle error
+			}
+		})();
+	}, [settings]);
+
+	if (loading) return null;
 
 	return (
 		<SettingsContext.Provider value={settings}>
