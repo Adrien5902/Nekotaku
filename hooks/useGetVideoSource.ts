534f9cbaa3b6e015f827be6ec71721a4c2f6ce4f
Adrien5902 <adrien.m5902@gmail.com>
2024-12-26 23:02:05 +0100

    added user agent which prevented video from loading when missing

--- hooks/useGetVideoSource.ts
+++ hooks/useGetVideoSource.ts
@@ -42,6 +42,7 @@ export const supportedLecteurs: Record<string, (url: string) => [Promise<string>
     "sibnet.ru": (url: string) => {
         const headers: Record<string, string> = {
             "Referer": url,
+            "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
         }
 
         return [(async () => {
