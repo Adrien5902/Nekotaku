96d1c323889c0b4720212aa98654ef98da22c1da
Adrien5902 <adrien.m5902@gmail.com>
2024-12-28 13:38:23 +0100

    loading and error handling on media followers

--- components/Media/MediaFollowing.tsx
+++ components/Media/MediaFollowing.tsx
@@ -1,8 +1,9 @@
 import { gql } from "@/types/Anilist";
 import type { Media } from "@/types/Anilist/graphql";
 import { useQuery } from "@apollo/client";
-import { ScrollView } from "react-native";
+import { RefreshControl, ScrollView } from "react-native";
 import OtherUserMediaListEntry from "./OtherUserMediaListEntry";
+import { ThemedText } from "../ThemedText";
 
 export interface Props {
 	media: Pick<Media, "id" | "episodes">;
@@ -29,21 +30,32 @@ export const FOLLOWING_MEDIA_LIST_QUERY = gql(`
 `);
 
 export default function MediaFollowing({ media }: Props) {
-	const { error, loading, data } = useQuery(FOLLOWING_MEDIA_LIST_QUERY, {
-		variables: {
-			id: media.id,
+	const { error, loading, data, refetch } = useQuery(
+		FOLLOWING_MEDIA_LIST_QUERY,
+		{
+			variables: {
+				id: media.id,
+			},
 		},
-	});
+	);
 
 	return (
-		<ScrollView>
-			{data?.Page?.mediaList?.map((mediaList) => (
-				<OtherUserMediaListEntry
-					media={media}
-					mediaList={mediaList}
-					key={mediaList?.id}
-				/>
-			))}
+		<ScrollView
+			refreshControl={
+				<RefreshControl refreshing={loading} onRefresh={refetch} />
+			}
+		>
+			{error ? (
+				<ThemedText>Error: {error.message}</ThemedText>
+			) : (
+				data?.Page?.mediaList?.map((mediaList) => (
+					<OtherUserMediaListEntry
+						media={media}
+						mediaList={mediaList}
+						key={mediaList?.id}
+					/>
+				))
+			)}
 		</ScrollView>
 	);
 }
